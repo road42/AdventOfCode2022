1bcca49a062abfd5e9e0f22badd325099bafd348
Christoph Jahn <christoph.jahn@scherdel.de>
2022-12-15 10:55:32 +0100

    Day 03 Part 2

--- 03/Program.cs
+++ 03/Program.cs
@@ -1,5 +1,9 @@
 ﻿
 var sumOfItems = 0;
+var sumOfGroups = 0;
+
+var groupCounter = 0;
+var group = new List<string>();
 
 // Read file
 foreach (string line in System.IO.File.ReadLines(@"input.txt"))
@@ -10,31 +14,68 @@ foreach (string line in System.IO.File.ReadLines(@"input.txt"))
     var firstHalf = cleanLine.Substring(0, amountOfItems / 2);
     var secondHalf = cleanLine.Substring(amountOfItems / 2);
 
+    groupCounter++;
+
+    // add line to elfGroupList
+    if (groupCounter <= 3)
+        group.Add(cleanLine);
+
     var foundDouble = "";
 
-    // Get shared items
-    foreach(var c in firstHalf)
+    // Get shared items (part 1)
+    foreach (var c in firstHalf)
     {
         if (secondHalf.Contains(c) && !foundDouble.Contains(c))
             foundDouble += c;
     }
 
-    var value = 0;
-    var asciiValue = (int) foundDouble.ToCharArray()[0];
+    // Get shared items (part 2)
+    if (groupCounter == 3)
+    {
+        var foundDouble2 = "";
+
+        // compare 1 to 2/3
+        foreach (var c in group[0])
+            if (group[1].Contains(c) && group[2].Contains(c) && !foundDouble2.Contains(c))
+                foundDouble2 += c;
+
+        // compare 2 to 3
+        foreach (var c in group[1])
+            if (group[0].Contains(c) && group[2].Contains(c) && !foundDouble2.Contains(c))
+                foundDouble2 += c;
+
+        foreach (var l in group)
+            Console.WriteLine($"L: {l}");
+
+        Console.WriteLine($"D: {foundDouble2}");
+        Console.WriteLine(".......................................");
+
+        sumOfGroups += getValueForChar(foundDouble2.ToCharArray()[0]);
+        groupCounter = 0;
+        group.Clear();
+    }
+
+    sumOfItems += getValueForChar(foundDouble.ToCharArray()[0]);
+}
+
+int getValueForChar(char c)
+{
+    var asciiValue = (int)c;
 
     // lower case
     if (asciiValue >= 97 && asciiValue <= 122)
     {
-        value = asciiValue - 96;
+        return asciiValue - 96;
     }
 
     // uppercase
     if (asciiValue >= 65 && asciiValue <= 90)
     {
-        value = asciiValue - 64 + 26;
+        return asciiValue - 64 + 26;
     }
 
-    sumOfItems += value;
+    return 0;
 }
 
-Console.WriteLine($"Sum: {sumOfItems}");
+Console.WriteLine($"SumItems: {sumOfItems}");
+Console.WriteLine($"SumGroups: {sumOfGroups}");
