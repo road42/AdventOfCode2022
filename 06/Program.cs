47838bc2aa6d63a4dac55d4312e7934468f4f46f
Christoph Jahn <christoph.jahn@scherdel.de>
2022-12-16 07:43:46 +0100

    Day 06 Part 1

---
+++ 06/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.IO;
+
+// Read File
+var line = File.ReadLines(@"input.txt").First();
+
+var search = new Queue<char>(5);
+var charCounter = 0;
+
+foreach (var c in line)
+{
+    search.Enqueue(c);
+    charCounter++;
+
+    if (search.Count == 5)
+        search.Dequeue();
+
+    var content = queueContentAsString(search);
+
+    if (search.Count == 4)
+    {
+        var letters = content?
+            .Distinct()
+            .Count();
+
+        if (letters == 4)
+        {
+            Console.WriteLine($"Found@ {charCounter} - Q: {content}");
+            break;
+        }
+    }
+}
+
+string queueContentAsString(IEnumerable<char> search)
+{
+    var ret = "";
+
+    foreach (var c in search)
+        ret += c;
+
+    return ret;
+}
